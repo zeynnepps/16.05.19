67b0f8e4154b26af67b2cdb89784b05ee53b5c2f
Zeynep <zeynepsalihoglu@gmail.com>
2019-05-16 08:27:18 -0300

    Initial commit

---
+++ quadrado.py
@@ -0,0 +1,17 @@
+import turtle
+
+t = turtle.Turtle()
+
+t.color('blue')
+
+for i in range(10):
+    t.forward(100)
+    t.left(90)
+    t.forward(10)
+    t.left(90)
+    t.forward(100)
+    t.right(90)
+    t.forward(10)
+    t.right(90)
+
+    
---
+++ tartarugastar.py
@@ -0,0 +1,16 @@
+
+import turtle
+
+t = turtle.Turtle()
+t.shape('turtle')
+t.color('green')
+t.speed(5)
+
+
+for i in range(20):
+    t.forward(i*10)
+    t.right(144)
+turtle.done()
+
+
+
